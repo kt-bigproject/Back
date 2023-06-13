78fc47f46be9a03bc23cefa9cd1a158ff9eba891
문석호 <quadoh@gmail.com>
2023-06-13 16:43:47 +0900

    cors close

--- sign/sign/settings.py
+++ sign/sign/settings.py
@@ -66,6 +66,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = '/rest-auth/login/'
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",	
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -73,7 +74,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",	
 ]	
 
 CORS_ALLOWED_ORIGINS = [
@@ -82,6 +82,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
+    "http://localhost:8000",
     "http://127.0.0.1:8000",
 ]
 
