2483e845c2d81286435d21f7d6db09717447859c
문석호 <quadoh@gmail.com>
2023-06-13 14:38:28 +0900

    Image-upload, closing old project

--- sign_1/sign/urls.py
+++ sign_1/sign/urls.py
@@ -4,6 +4,14 @@ from django.views.generic import TemplateView
 from rest_auth.views import LoginView, LogoutView, PasswordChangeView
 from rest_auth.registration.views import VerifyEmailView, RegisterView
 from sign.views import ConfirmEmailView
+from rest_framework.routers import DefaultRouter
+from practice import views
+from django.views.static import serve
+from django.conf import settings	
+from django.conf.urls.static import static
+
+router = DefaultRouter()
+router.register('PracticeContent', views.PracticeContentView, 'PracticeContent')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,5 +34,8 @@ urlpatterns = [
 
     # 유저가 클릭한 이메일(=링크) 확인
     re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(), name='account_confirm_email'),
+    
+    # 글씨 연습 이미지 업로드
+    path('practice/', include('practice.urls')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- sign_1/sign/views.py
+++ sign_1/sign/views.py
@@ -10,26 +10,6 @@ from rest_framework.permissions import AllowAny
 from allauth.account.models import EmailConfirmation, EmailConfirmationHMAC
 
 
-# class ConfirmEmailView(APIView):
-#     permission_classes = [AllowAny]
-#
-#     def get(self, request, *args, **kwargs):
-#         confirmation = self.get_object()
-#         confirmation.confirm(self.request)
-#         # 성공 시나리오는 React Router Route가 처리할 것입니다.
-#         return Response({'message': 'Email confirmed successfully.'})
-#
-#     def get_object(self):
-#         key = self.kwargs['key']
-#         email_confirmation = EmailConfirmationHMAC.from_key(key)
-#         if not email_confirmation:
-#             raise NotFound('Email confirmation not found.')
-#         return email_confirmation
-#
-#     def get_queryset(self):
-#         qs = EmailConfirmation.objects.all_valid()
-#         qs = qs.select_related("email_address__user")
-#         return qs
 class ConfirmEmailView(APIView):
     permission_classes = [AllowAny]
 
