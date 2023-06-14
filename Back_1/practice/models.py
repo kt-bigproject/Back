a472a8ef931af82462a5eb6253a0afff1b191df6
문석호 <quadoh@gmail.com>
2023-06-14 15:30:09 +0900

    sentence upload by admin

--- Back_1/practice/models.py
+++ Back_1/practice/models.py
@@ -5,3 +5,7 @@ class PracticeContent(models.Model):
     id = models.AutoField(primary_key=True, null=False, blank=False)
     font = models.CharField(max_length=30, null=True)
     image = models.ImageField(blank=True, null=True, upload_to="uploads")
+    
+class SentenceContent(models.Model):
+    id = models.AutoField(primary_key=True, null=False, blank=False)
+    sentence = models.CharField(max_length=30, null=True)
--- Back_1/practice/serializers.py
+++ Back_1/practice/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import PracticeContent
+from .models import PracticeContent, SentenceContent
 
 class PracticeContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = PracticeContent
+        fields = '__all__'
+        
+class SentenceContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SentenceContent
         fields = '__all__'
