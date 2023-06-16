3d2bf7b885701421c90feb2ff59ef4736837f646
문석호 <quadoh@gmail.com>
2023-06-16 15:58:45 +0900

    front

--- Back_1/predict/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from predict.models import Predict_Result
-
-
-class MyPredictSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Predict_Result
-        fields ='__all__'
