dedb72825f58d3ba2c855194c2a62712331e0484
문석호 <quadoh@gmail.com>
2023-06-16 11:28:15 +0900

    auth temp off

--- Back_1/blog/views.py
+++ Back_1/blog/views.py
@@ -4,12 +4,12 @@ from rest_framework import viewsets
 # from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import IsOwnerOrReadOnly
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+# from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 # (게시글) Blog의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class BlogViewSet(viewsets.ModelViewSet):
-    authentication_classes = [JSONWebTokenAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    # authentication_classes = [JSONWebTokenAuthentication]
+    # permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
    
@@ -18,8 +18,8 @@ class BlogViewSet(viewsets.ModelViewSet):
 
 # (댓글) Comment 보여주기, 수정하기, 삭제하기 모두 가능
 class CommentViewSet(viewsets.ModelViewSet):
-    authentication_classes = [JSONWebTokenAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    # authentication_classes = [JSONWebTokenAuthentication]
+    # permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
