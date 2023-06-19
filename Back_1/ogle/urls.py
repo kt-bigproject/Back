7ee94e5a3eda41ca44de273e6230041745b32ea6
문석호 <quadoh@gmail.com>
2023-06-19 11:11:41 +0900

    temp board

--- Back_1/ogle/urls.py
+++ Back_1/ogle/urls.py
@@ -6,14 +6,19 @@ from rest_framework.routers import DefaultRouter
 from practice import views
 
 router = DefaultRouter()
-router.register('PracticeContent', views.PracticeContentView, 'PracticeContent')
+# router.register('PracticeContent', views.PracticeContentView, 'PracticeContent')
+router.register('PracticeContent', views.PracticeContentView, basename='PracticeContent')
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('rest-auth/', include('sign.urls')),
+    # path('rest-auth/', include('sign.urls')),
+    path('rest-auth/', include(('sign.urls', 'sign'), namespace='rest_auth')),
     path('blog/', include('blog.urls')),
     # 글씨 연습 이미지 업로드
     path('practice/', include('practice.urls')),
+    # 권한 연습용 임시 게시판
+    path('temp/', include('temp.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
