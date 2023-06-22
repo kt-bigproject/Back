2e959df5690f55ab2a91059234fb28c417bf199e
문석호 <quadoh@gmail.com>
2023-06-22 14:48:10 +0900

    Remove __pycache__/ and *.pyc files

--- Back_1/blog/serializer.py
+++ Back_1/blog/serializer.py
@@ -3,13 +3,13 @@ from .models import Blog, Comment
 from rest_framework import serializers
 
 class BlogSerializer(serializers.ModelSerializer):
-    # user = serializers.ReadOnlyField(source = 'user.username', allow_null=True)
+    user = serializers.ReadOnlyField(source = 'user.username', allow_null=True)
     class Meta:
         model = Blog
         fields = ['id', 'title', 'created_at', 'user', 'body', 'image']
 
 class CommentSerializer(serializers.ModelSerializer):
-    # user = serializers.ReadOnlyField(source = 'user.nickname')
+    user = serializers.ReadOnlyField(source = 'user.username')
     class Meta:
         model = Comment
         fields = ['id', 'blog', 'user', 'created_at', 'comment']
