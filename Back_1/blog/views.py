9f6154b3a48610529594a11d8312896776533c8f
platypus012 <platypus012@gmail.com>
2023-06-22 14:44:39 +0900

    blog_to_temp

--- Back_1/blog/views.py
+++ Back_1/blog/views.py
@@ -1,20 +1,21 @@
 from .models import Blog, Comment
 from .serializer import BlogSerializer, CommentSerializer
 from rest_framework import viewsets
-# from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .permissions import IsOwnerOrReadOnly
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+# from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+#from rest_framework.permissions import IsAuthenticatedOrReadOnly
+#from .permissions import IsOwnerOrReadOnly
 # from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-from rest_framework.pagination import PageNumberPagination
 
 class CustomPageNumberPagination(PageNumberPagination):
     page_size = 8
     
 # (게시글) Blog의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class BlogViewSet(viewsets.ModelViewSet):
-    # authentication_classes = [JSONWebTokenAuthentication]
+    authentication_classes = [JWTAuthentication]
     # permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Blog.objects.all().order_by('-created_at')
     serializer_class = BlogSerializer
@@ -25,12 +26,12 @@ class BlogViewSet(viewsets.ModelViewSet):
 
 # (댓글) Comment 보여주기, 수정하기, 삭제하기 모두 가능
 class CommentViewSet(viewsets.ModelViewSet):
-    # authentication_classes = [JSONWebTokenAuthentication]
+    authentication_classes = [JWTAuthentication]
     # permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['blog']
 
-    # def perform_create(self, serializer):
-    #     serializer.save(user = self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(user = self.request.user)
