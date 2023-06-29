5e768a9cfea4dd018d038a6297f36224579047a8
문석호 <quadoh@gmail.com>
2023-06-29 14:47:22 +0900

    temp del

--- Back_1/temp/serializer.py
+++
@@ -1,9 +0,0 @@
-# temp/serializer.py
-from .models import Temp
-from rest_framework import serializers
-
-class TempSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source = 'user.username', allow_null=True)
-    class Meta:
-        model = Temp
-        fields = ['id', 'title', 'created_at', 'user', 'body', 'image']
